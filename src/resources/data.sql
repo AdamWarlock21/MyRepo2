267e1fb50ece3b5ce353c258943486b624df9db8
AdamWarlock21 <47519717+AdamWarlock21@users.noreply.github.com>
2019-10-21 08:59:14 +0200

    database interface

---
+++ src/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('Ismail', 'Vardien', '213180545@mycput.ac.za'),
+  	('Aneeqa', 'Abrahams', '3561438@myuwc.ac.za'),
+  	('Tauriq', 'Hendricks', '215067290@mycput.ac.za'),
+  	('Notur', 'Mama', '31548668@uct.ac.za'),
+  	('Mike', 'Oxmaul', '3254892@myuwc.ac.za'),
+  	('Goin', 'Ham', '254868@email.com');
